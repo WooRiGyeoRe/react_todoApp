7426378f2b789c08dbfd94ef4d98fece9590dfe8
WooRiGyeoRe <rufp0607@naver.com>
2024-11-27 15:27:31 +0900

    TodoInsert에서 onSubmit 이벤트 설정

--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -6,7 +6,7 @@ import './TodoInsert.scss';
 // 사이트에서 사용하고 싶은 아이콘을 고르고, import 구문을 사용하여 불러옴
 // 예시 : import { 아이콘 이름 } from 'react-icons/md';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   // value 상태 정의 (input 입력값 관리)
   const [value, setValue] = useState('');
 
@@ -16,8 +16,20 @@ const TodoInsert = () => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value 값 초기화
+
+      // submit 이벤트 => 브라우저 새로고침 발생시킴
+      // 이를 방지하기 위한 함수 호출
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       {/* placeholder => 입력 필드(input, textarea 등)의 힌트 텍스트 */}
       <input
         placeholder="할 일을 입력하세요"
