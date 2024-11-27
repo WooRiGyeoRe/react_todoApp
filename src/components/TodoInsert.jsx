4765c57935447d31b304355d5a1cbd10c8a5dbf3
WooRiGyeoRe <rufp0607@naver.com>
2024-11-27 14:48:57 +0900

    TodoInsert value 상태 관리하기

--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md'; // 아이콘 사용
 import './TodoInsert.scss';
 
@@ -7,10 +7,23 @@ import './TodoInsert.scss';
 // 예시 : import { 아이콘 이름 } from 'react-icons/md';
 
 const TodoInsert = () => {
+  // value 상태 정의 (input 입력값 관리)
+  const [value, setValue] = useState('');
+
+  // input에 넣을 함수
+  // 재사용을 위해 useCallback 훅 사용
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form className="TodoInsert">
       {/* placeholder => 입력 필드(input, textarea 등)의 힌트 텍스트 */}
-      <input placeholder="할 일을 입력하세요" />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
