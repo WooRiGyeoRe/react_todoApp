a783d6573f37290d848667e0a6ff3f31941e64f7
WooRiGyeoRe <rufp0607@naver.com>
2024-11-27 13:24:50 +0900

    UI 구성_TodoInsert 만들기

---
+++ src/components/TodoInsert.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MdAdd } from 'react-icons/md'; // 아이콘 사용
+
+// [ 아이콘 사용 방법 ]
+// 사이트에서 사용하고 싶은 아이콘을 고르고, import 구문을 사용하여 불러옴
+// 예시 : import { 아이콘 이름 } from 'react-icons/md';
+
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      {/* placeholder => 입력 필드(input, textarea 등)의 힌트 텍스트 */}
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TodoTemplate.scss';
 
+// children => 컴포넌트 내부에 전달된 자식 요소
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
