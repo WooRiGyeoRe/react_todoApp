729a629d0ada4db909792050d48ad5407e7fe089
WooRiGyeoRe <rufp0607@naver.com>
2024-11-27 13:30:10 +0900

    TodoListItem.jsx 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoListItem from './components/TodoListItem';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
     </TodoTemplate>
   );
 };
